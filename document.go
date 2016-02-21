2f4f6059dc0d869b9f3c0e886bee4e823d0908c3
Shun Takebayashi <shun@takebayashi.asia>
2016-02-21 18:30:59 +0900

    Support UTF-8

--- document.go
+++ document.go
@@ -2,24 +2,13 @@ package npbbis
 
 import (
 	"github.com/PuerkitoBio/goquery"
-	"golang.org/x/text/encoding/japanese"
-	"golang.org/x/text/transform"
-	"io"
 	"net/http"
 )
 
 func newGoqueryDocument(uri string) (*goquery.Document, error) {
-	reader, err := createShiftJISReader(uri)
-	if err != nil {
-		return nil, err
-	}
-	return goquery.NewDocumentFromReader(reader)
-}
-
-func createShiftJISReader(uri string) (io.Reader, error) {
 	res, err := http.Get(uri)
 	if err != nil {
 		return nil, err
 	}
-	return transform.NewReader(res.Body, japanese.ShiftJIS.NewDecoder()), nil
+	return goquery.NewDocumentFromReader(res.Body)
 }
